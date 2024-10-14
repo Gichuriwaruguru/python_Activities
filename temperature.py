e01c23f8eda7ebf506f7a8a3591104e2ad1c188d
Gichuriwaruguru <113664241+Gichuriwaruguru@users.noreply.github.com>
2024-10-14 06:58:52 -0700

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,18 @@
+#
+class Temparature:
+    def __init__(self, celcius, farenheit):
+        self.celcius=celcius
+        self.farenheit=farenheit
+    def convertFarenhiet(self):
+        return (self.celcius * 9/5)+32
+    def convertCelcius(self):
+        return (self.farenheit -32) * 5/9
+        
+    
+celcius=int(input("Degree celcius "))
+farenheit=int(input("Farenheit"))
+temp= Temparature(celcius=25, farenheit=78)
+
+print(f"The farenhiet is {temp.convertFarenhiet()}")
+print(f"The celcius is {temp.convertCelcius()}")
+        
